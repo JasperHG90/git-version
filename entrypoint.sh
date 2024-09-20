98a9928824c578dfa46bfc14d971476434fa9cc4
Jasper Ginn <jasperginn@gmail.com>
2024-09-20 11:22:00 +0200

    build: fail on bad exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
-git config --global --add safe.directory /github/workspace
+#git config --global --add safe.directory /github/workspace
 VERSION=$(/.venv/bin/git_version version $1 --path-to-pyproject $2)
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  echo "Failed to get version"
+  exit $exit_code
+fi
 echo "Version: $VERSION"
 echo "VERSION=$VERSION" >> $GITHUB_ENV
