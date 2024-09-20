cb7d5228117cde81a008da94a63ea7b45f170c8d
Jasper Ginn <jasperginn@gmail.com>
2024-09-20 11:15:21 +0200

    chore: add git version safe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+git config --global --add safe.directory /github/workspace
 VERSION=$(/.venv/bin/git_version version $1 --path-to-pyproject $2)
 echo "Version: $VERSION"
 echo "VERSION=$VERSION" >> $GITHUB_ENV
